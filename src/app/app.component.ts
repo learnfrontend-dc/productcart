01d36082468549660b936bcdf4c1394d37a10f35
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-05-15 20:40:17 -0500

    window post message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,71 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, TemplateRef, Renderer2, OnDestroy, OnInit } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+
+export class Product {
+  public productname:string;
+  public code: string;
+  public price: number;
+  public rating: string;
+  public qty: number
+  constructor(){
+   
+  }
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+export class AppComponent implements OnInit, OnDestroy {
+  windowListener: Function;
 
-  public openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
+  public productlist : Product[]; 
+  public totalprice=0;
+  public ngOnInit(): void {
+    this.productlist=[];
   }
+  constructor(private renderer: Renderer2) {
+    this.windowListener =
+        renderer.listen('window', 'message', this.processMessage.bind(this));
+  }
+
+  processMessage(event: Event) {
+    const message = event as MessageEvent;
+    let product=this.getProduct(message.data['productname']);
+    if(product !== undefined) {
+      product.qty=product.qty + 1;
+      product.price=product.price+message.data['price'];
+     } else {
+      product = new Product();
+      product.qty=1;
+      product.price=message.data['price'];
+      product.productname=message.data['productname'];
+      this.productlist.push(product);
+    }
+    this.totalprice=this.totalprice+product.price;
+  }
+
+  getProduct(productname) : Product {
+    let productObj=undefined;
+    for(let product of this.productlist) {
+      if(product.productname === productname) {
+        productObj=product; 
+        break;
+      }
+  }
+  return productObj;
+  }
+  ngOnDestroy() {
+    this.windowListener();
+  }
+
+  
 }
+
+//window.addEventListener('message', function(e) {
+  //var message = e.data;
+  //alert(message);
+//});
